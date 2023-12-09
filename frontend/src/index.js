b54a8691b0f4e8c02c429347189291a22e3b93c5
Parth Patel <parth_m_patel@apple.com>
2023-12-09 00:48:55 -0800

    pressing p highlights the video

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,5 +1,3 @@
-// App.js
-
 import React, { useState, useEffect } from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
@@ -14,10 +12,9 @@ import { KirtanProvider } from './useKirtanContext';
 import VideoBackground from './Components/VideoBackground';
 
 const App = () => {
-  const [selectedVideoId, setSelectedVideoId] = useState(''); // Default video ID
+  const [selectedVideoId, setSelectedVideoId] = useState('');
 
   useEffect(() => {
-    // Fetch the video ID from the server when the component mounts
     const fetchVideoId = async () => {
       try {
         const response = await fetch(`${process.env.REACT_APP_BACKEND_URL}/api/getVideoId`);
@@ -31,6 +28,35 @@ const App = () => {
     fetchVideoId();
   }, []);
 
+  useEffect(() => {
+    const handleKeyPress = (event) => {
+      if (event.key === 'p') {
+        // Fade out Deck component
+        const deck = document.querySelector('.reveal'); // replace with the actual class or ID of your Deck component
+        deck.style.transition = 'opacity 1s';
+        const videoBackground = document.querySelector('.video-background');
+        videoBackground.style.transition = 'opacity 1s';
+
+        if (deck.style.opacity == 1) {
+          deck.style.opacity = 0;
+          // Fade in video background
+          videoBackground.style.opacity = 1;
+        } else {
+          deck.style.opacity = 1;
+          videoBackground.style.opacity = 0.7
+        }
+
+        
+      }
+    };
+
+    document.addEventListener('keydown', handleKeyPress);
+
+    return () => {
+      document.removeEventListener('keydown', handleKeyPress);
+    };
+  }, []); // Empty dependency array means this effect will run once when the component mounts
+
   return (
     <Router>
       <Routes>
@@ -41,7 +67,7 @@ const App = () => {
             <div className="App">
               <KirtanProvider>
                 <VideoBackground videoId={selectedVideoId} />
-                <Deck>{Slides}</Deck>
+                <Deck className="deck-class">{Slides}</Deck>
               </KirtanProvider>
             </div>
           }
