4d504bea795f9f9f00dd581cde9882b7989de271
Parth Patel <parth_m_patel@apple.com>
2023-12-09 00:27:44 -0800

    better admin page

--- frontend/src/Components/AdminPage.js
+++ frontend/src/Components/AdminPage.js
@@ -1,5 +1,6 @@
 import React, { useState } from 'react';
 import axios from 'axios';
+import styled from 'styled-components';
 
 const AdminPage = () => {
   const [inputText, setInputText] = useState('');
@@ -7,16 +8,16 @@ const AdminPage = () => {
   const [textSubmitted, setTextSubmitted] = useState(false);
   const [videoId, setVideoId] = useState('');
 
+  const handleInputChange = (e) => {
+    setInputText(e.target.value);
+    setFeedback(null);
+    setTextSubmitted(false);
+  };
+
   const handleVideoInputChange = (e) => {
     setVideoId(e.target.value);
     setFeedback(null);
     setTextSubmitted(false);
-  }
-
-  const handleInputChange = (e) => {
-    setInputText(e.target.value);
-    setFeedback(null);
-    setTextSubmitted(false); // Clear previous success message when input changes
   };
 
   const handleShowButtonClick = async () => {
@@ -26,7 +27,7 @@ const AdminPage = () => {
       });
       setFeedback({ type: 'success', message: 'Text sent successfully' });
       setInputText('');
-      setTextSubmitted(true); // Set to true on successful submission
+      setTextSubmitted(true);
     } catch (error) {
       setFeedback({ type: 'error', message: `Error sending text: ${error.message}` });
     }
@@ -35,113 +36,101 @@ const AdminPage = () => {
   const handleSaveButtonClick = async () => {
     try {
       await axios.post(`${process.env.REACT_APP_BACKEND_URL}/api/saveVideoId`, {
-        videoId: videoId, // Use the correct variable
+        videoId: videoId,
       });
       setFeedback({ type: 'success', message: 'Video ID saved successfully' });
     } catch (error) {
       setFeedback({ type: 'error', message: `Error saving video ID: ${error.message}` });
     }
   };
-  
 
   return (
-    <div style={styles.container}>
-      <div style={styles.content}>
-        <h2 style={styles.heading}>Admin Page</h2>
-        <div style={styles.inputContainer}>
-          <label style={styles.label}>
-            Enter Text:
-            <br />
-            <textarea
+    <Container>
+      <Content>
+        <Heading>Presentation Controls</Heading>
+        <InputContainer>
+          <InputLabel>
+            Enter footer text:
+            <Textarea
               rows="4"
               cols="30"
               value={inputText}
               onChange={handleInputChange}
-              style={styles.textarea}
             />
-          </label>
-          <button onClick={handleShowButtonClick} style={styles.button}>
-            Show Text
-          </button>
-        </div>
-        <div style={styles.inputContainer}>
-          <label style={styles.label}>
+          </InputLabel>
+          <Button onClick={handleShowButtonClick}>Show Text</Button>
+        </InputContainer>
+        <InputContainer>
+          <InputLabel>
             Enter Video ID:
-            <br />
-            <textarea
+            <Textarea
               rows="4"
               cols="10"
               value={videoId}
               onChange={handleVideoInputChange}
-              style={styles.textarea}
             />
-          </label>
-          <button onClick={handleSaveButtonClick} style={styles.button}>
-            Change video
-          </button>
-        </div>
-        {feedback && (
-          <div style={styles.feedback} className={feedback.type}>
-            {feedback.message}
-          </div>
-        )}
-        {textSubmitted && <div style={styles.textSubmitted}>Text Submitted</div>}
-      </div>
-    </div>
+          </InputLabel>
+          <Button onClick={handleSaveButtonClick}>Change Video</Button>
+        </InputContainer>
+        {feedback && <Feedback className={feedback.type}>{feedback.message}</Feedback>}
+        {textSubmitted && <TextSubmitted>Text Submitted</TextSubmitted>}
+      </Content>
+    </Container>
   );
 };
 
-const styles = {
-  container: {
-    display: 'flex',
-    justifyContent: 'center',
-    alignItems: 'center',
-    height: '100vh',
-  },
-  content: {
-    textAlign: 'center',
-  },
-  heading: {
-    fontSize: '24px',
-    marginBottom: '10px',
-  },
-  inputContainer: {
-    display: 'flex',
-    flexDirection: 'column',
-    maxWidth: '300px',
-    margin: 'auto',
-  },
-  label: {
-    marginBottom: '10px',
-  },
-  textarea: {
-    padding: '8px',
-    marginBottom: '10px',
-  },
-  button: {
-    padding: '8px',
-    background: '#007bff',
-    color: '#fff',
-    cursor: 'pointer',
-  },
-  feedback: {
-    marginTop: '10px',
-    padding: '8px',
-    borderRadius: '4px',
-    color: '#fff',
-    fontWeight: 'bold',
-  },
-  success: {
-    background: '#28a745',
-  },
-  error: {
-    background: '#dc3545',
-  },
-  textSubmitted: {
-    marginTop: '10px',
-    color: '#007bff',
-    fontWeight: 'bold',
-  },
-};
+const Container = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  height: 100vh;
+`;
+
+const Content = styled.div`
+  text-align: center;
+`;
+
+const Heading = styled.h2`
+  font-size: 24px;
+  margin-bottom: 10px;
+`;
+
+const InputContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  max-width: 300px;
+  margin: auto;
+  margin-bottom: 20px;
+`;
+
+const InputLabel = styled.label`
+  margin-bottom: 10px;
+`;
+
+const Textarea = styled.textarea`
+  padding: 8px;
+  margin-bottom: 10px;
+`;
+
+const Button = styled.button`
+  padding: 8px;
+  background: #007bff;
+  color: #fff;
+  cursor: pointer;
+`;
+
+const Feedback = styled.div`
+  margin-top: 10px;
+  padding: 8px;
+  border-radius: 4px;
+  color: #fff;
+  font-weight: bold;
+`;
+
+const TextSubmitted = styled.div`
+  margin-top: 10px;
+  color: #007bff;
+  font-weight: bold;
+`;
 
 export default AdminPage;
